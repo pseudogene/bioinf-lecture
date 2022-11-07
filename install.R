03a6da85ec07b056a928080d505876e89b9fb961
Michaël Bekaert <pseudogene@users.noreply.github.com>
2022-11-07 15:02:10 +0000

    Update install.R

--- install.R
+++ install.R
@@ -1,7 +1,7 @@
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager");
 BiocManager::install("DESeq2");
-BiocManager::install("EnhancedVolcano");
+BiocManager::install('EnhancedVolcano', type = 'source');
 install.packages("ashr", quietly = TRUE);
 install.packages("pheatmap", quietly = TRUE);
 install.packages("ggplot2", quietly = TRUE);
